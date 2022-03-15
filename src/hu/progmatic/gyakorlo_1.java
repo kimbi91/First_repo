229a14053171e9d20e3ffed049d1407263c67db3
kimbi91 <subscribefan@gmail.com>
2022-03-15 20:02:34 +0100

    gyakorlo 1

---
+++ src/hu/progmatic/gyakorlo_1.java
@@ -0,0 +1,131 @@
+package hu.progmatic;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class gyakorlo_1 {
+
+    public static void main(String[] args) {
+
+        // Nullával nem osztunk!
+
+        Scanner sc = new Scanner(System.in);
+/*
+        System.out.println("Kérem az első számot!");
+        int firstNumber = sc.nextInt();
+
+        System.out.println("Kérem a második számot!");
+        int secondNumber = sc.nextInt();
+
+
+        if (secondNumber == 0) {
+            System.out.println("Nullával nem osztunk!");
+        } else  {
+            System.out.println(firstNumber / secondNumber);
+        }
+
+        // Szám csökkentése, ha.....
+
+        int number = -1;
+
+        System.out.println(number>0 ? --number : number);
+
+
+
+        // Negatív vagy sem?
+
+        System.out.println("Kérem a számot!");
+        int number = sc.nextInt();
+
+        if (number == 0) {
+            System.out.println("Nulla");
+        } else if (number > 0) {
+            System.out.println("Pozitív");
+        } else {
+            System.out.println("Negatív");
+        }
+
+
+        // Közlekedési lámpa
+
+        String trafficLight = "kiscica";
+
+        if (trafficLight.equalsIgnoreCase("zöld")) {
+            System.out.println("SZABAD");
+        } else if (trafficLight.equalsIgnoreCase("sárga")) {
+            System.out.println("LASSÍTS");
+        } else if (trafficLight.equalsIgnoreCase("piros")) {
+            System.out.println("MEGÁLLJ");
+        } else {
+            System.out.println("NEM TUDOM");
+        }
+
+
+        // Évszakok
+
+        String month = "kiscica";
+
+        if (month.equalsIgnoreCase("december") || month.equalsIgnoreCase("január") || month.equalsIgnoreCase("február")) {
+            System.out.println("tél");
+        } else if (month.equalsIgnoreCase("március") || month.equalsIgnoreCase("április") || month.equalsIgnoreCase("május")) {
+            System.out.println("tavasz");
+        } else if (month.equalsIgnoreCase("június") || month.equalsIgnoreCase("július") || month.equalsIgnoreCase("augusztus")) {
+            System.out.println("nyár");
+        } else if (month.equalsIgnoreCase("szeptember") || month.equalsIgnoreCase("október") || month.equalsIgnoreCase("november")) {
+            System.out.println("ősz");
+        } else {
+            System.out.println("nem meghatározható");
+        }
+
+
+        // Számok kiírása
+
+        int n = 10;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+
+
+        // Visszaszámlálás
+
+        int n = 10;
+
+        for (int i = n; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+
+        // Faktoriális
+
+        int n = 10;
+
+        for (int i = n - 1; i > 0; i--) {
+            n *= i;
+        }
+
+        System.out.println("Faktoriálisa: " + n);
+
+
+        // ABC
+
+        int n = 26;
+
+        String result = "";
+
+        for (int i = 0; i < n; i++) {
+            char charToAdd = (char) ('a' + i);
+            result += charToAdd;
+        }
+        System.out.println(result);
+
+        // Tömb szöveggé
+
+
+        int[] numbers = {-33, 66, 42, -13, 54, 89};
+
+        System.out.println(Arrays.toString(numbers));
+
+ */
+    }
+}
